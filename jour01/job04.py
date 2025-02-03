dd5611260da43dc996ba8de1198c91f68d6b330a
MrRobot <olivier.portal@laplateforme.io>
2025-02-03 11:28:44 +0100

    fix: add default value in job04

--- jour01/job04.py
+++ jour01/job04.py
@@ -3,9 +3,9 @@ class Personne:
         self.prenom = prenom
         self.nom = nom
         
-    def se_presenter(prenom, nom):
+    def se_presenter(prenom = "Olivier", nom = "Portal"):
         print(f"Je suis {prenom} {nom}")
         
-      
+Personne.se_presenter()      
 personne1 = Personne.se_presenter("John", "Doe")
 personne2 = Personne.se_presenter("Jean", "Dupond")
