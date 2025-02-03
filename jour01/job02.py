f9444d26ba4478613ad17d8d1f5fdf86cc6ebc7e
MrRobot <olivier.portal@laplateforme.io>
2025-02-03 10:04:41 +0100

    feat: job02 ok

---
+++ jour01/job02.py
@@ -0,0 +1,8 @@
+class Operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+        
+nombre = Operation(12, 3)
+print(f"Le nombre1 est {nombre.nombre1}")
+print(f"Le nombre2 est {nombre.nombre2}") 
