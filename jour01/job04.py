7086ed408149a288148514c44a284354738b8ab2
MrRobot <olivier.portal@laplateforme.io>
2025-02-03 12:08:38 +0100

    feature:simplify code in job04

--- jour01/job04.py
+++ jour01/job04.py
@@ -1,11 +1,11 @@
 class Personne:
-    def __init__(self, prenom, nom):
+    def __init__(self, prenom = "Olivier", nom = "Portal"):
         self.prenom = prenom
         self.nom = nom
         
-    def se_presenter(prenom = "Olivier", nom = "Portal"):
-        print(f"Je suis {prenom} {nom}")
+    def se_presenter(presenter):
+        print(f"Je suis {presenter.prenom} {presenter.nom}")
         
-Personne.se_presenter()      
-personne1 = Personne.se_presenter("John", "Doe")
-personne2 = Personne.se_presenter("Jean", "Dupond")
+Personne().se_presenter()      
+Personne("John", "Doe").se_presenter()
+Personne("Jean", "Dupond").se_presenter()
