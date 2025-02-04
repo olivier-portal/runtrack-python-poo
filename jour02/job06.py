f92cac6842a0f05bcd9fa6367fdf24a7b3300ffb
MrRobot <olivier.portal@laplateforme.io>
2025-02-04 16:55:27 +0100

    feat: job06 ok

--- jour02/job06.py
+++ jour02/job06.py
@@ -24,14 +24,48 @@ class Commande:
         
 # define method to add a dish
 
-    def ajout_plat(self, numero_commande, plat, prix, statut):
-        self.__liste_plats_commandés.append({"numéro de commande": numero_commande, "nom": plat, "prix": prix, "statut": statut})
+    def ajout_plat(self, plat, prix, statut):
+        self.__liste_plats_commandés.append({"nom": plat, "prix": prix, "statut": statut})
+        
+# define method to cancel a dish
+
+    def annuler_commande(self):
+        if self.__statut_commande == "en cours":
+            self.__statut_commande = "annulée"
+            print(f"Votre commande est {self.__statut_commande[2]}")
+        else:
+            print(f"Vous ne pouvez pas annulé une commande {self.__statut_commande[1]}")
+            
+# define a method to calculate the total price
+
+    def __calcul_prix(self):
+        total = 0
+        for plat in self.__liste_plats_commandés:
+            if plat["statut"] != "annulée":
+                total += plat["prix"]
+        return total
+            
+# define a method to show the priceetotal price to pay
+
+    def afficher_commande(self):
+        print(f"La commande {self.get_liste_plats_commandés()} est de {self.__calcul_prix()}€, avec {self.calcul_TVA()}€ de TVA")
+ 
+ # define method to calculate the amount of TVA       
+    def calcul_TVA(self):
+        return self.__calcul_prix() * 20/100
 
 commande_client = Commande()
 
 commande_client.set_statut_commande("terminée")  
-commande_client.ajout_plat(commande_client.numero_commande(), "Tajine", 12, commande_client.get_statut_commande())
+commande_client.ajout_plat("Tajine", 12, commande_client.get_statut_commande())
 print(commande_client.get_liste_plats_commandés())
 
-commande_client.ajout_plat(commande_client.numero_commande(), "Couscous Royal", 15, commande_client.get_statut_commande())
+commande_client.set_statut_commande("en cours") 
+commande_client.ajout_plat("Couscous Royal", 15, commande_client.get_statut_commande())
 print(commande_client.get_liste_plats_commandés())
+
+commande_client.set_statut_commande("annulée") 
+commande_client.ajout_plat("Couscous Royal", 15, commande_client.get_statut_commande())
+print(commande_client.get_liste_plats_commandés())
+
+print(commande_client.afficher_commande())
