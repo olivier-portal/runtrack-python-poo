2950298b035a07a270c14decae89e6081a0d0179
MrRobot <olivier.portal@laplateforme.io>
2025-02-04 14:36:25 +0100

    feat: job04 part 2 ok

--- jour02/job04.py
+++ jour02/job04.py
@@ -4,6 +4,7 @@ class Student:
         self.__firstname = firstname
         self.__student_number = student_number
         self.__credit_number = 0
+        self.__level = self.__student_eval()
         
     def get_add_credits(self):
         return self.__credit_number
@@ -11,8 +12,10 @@ class Student:
     def set_add_credits(self, credits):
         if type(credits) is int and credits > 0:
             self.__credit_number += credits
+            self.__level = self.__student_eval()
+            return self.__credit_number
         else:
-            print("The number of credits must be positive")
+            print("Le nombre de crédits doit être positif")
             
     def get_student_firstname(self):
         return self.__firstname
@@ -22,11 +25,35 @@ class Student:
     
     def get_student_number(self):
         return self.__student_number
+    
+    def __student_eval(self):
+        if self.__credit_number >= 0 and self.__credit_number < 60:
+            return "Insuffisant"
+        elif self.__credit_number >= 60 and self.__credit_number < 70:
+            return "Passable"
+        elif self.__credit_number >= 70 and self.__credit_number < 80:
+            return "Bien"
+        elif self.__credit_number >= 80 and self.__credit_number < 90:
+            return "Très bien"
+        elif self.__credit_number >= 90:
+            return "Excellent"
+            
+    def student_info(self):
+        print(f"\nNom = {self.__lastname}\nPrénom = {self.__firstname}\nid = {self.__student_number}\nNiveau = {self.__level}")
+        
             
 student1 = Student("John", "Doe", 145)
 
-student1.set_add_credits(10)
+student1.set_add_credits(-10)
+student1.set_add_credits(-10)
+student1.set_add_credits(-10)
+
+print(f"Le nombre de crédit de {student1.get_student_firstname()} {student1.get_student_lastname()} est de {student1.get_add_credits()} points")
+
+student1.student_info()
+
+student1.set_add_credits(30)
 student1.set_add_credits(10)
 student1.set_add_credits(10)
 
-print(f"Le nombre de crédit de {student1.get_student_firstname()} {student1.get_student_lastname()} est de {student1.get_add_credits()} points")
+student1.student_info()
